58e213436ce4c25e85952da2055444aa16dfa791
saifur703 <saifur703@gmail.com>
2020-09-18 18:05:59 +0600

    task completed

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,23 +1,13 @@
-(function($){
-	"use strict";
-	
-	jQuery(document).ready(function($){
-		$('.testimonial_author').owlCarousel({
-            items: 1,
-            margin: 30,
-            loop: false,
-            dots: false,
-            nav: false,
-            navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
-            autoplay: false,
-            smartSpeed: 500,
-            animateOut: "fadeOut"
-        });
-	});
-	
-	
-	jQuery(window).on("load", function(){
-		
-	});
-	
+(function ($) {
+  'use strict';
+
+  jQuery(document).ready(function ($) {
+    $('.mobile-menu').on('click', function () {
+      $(this).toggleClass('active');
+    });
+  });
+
+  jQuery(window).on('load', function () {
+    // Code
+  });
 })(jQuery);
